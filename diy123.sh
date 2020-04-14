724baf53a674db083669318ca40b83dd2773c437
zzs01 <36794188+zzs01@users.noreply.github.com>
2020-04-14 21:56:13 +0800

    Update diy123.sh

--- diy123.sh
+++ diy123.sh
@@ -8,7 +8,11 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
+# Modify hostname
 sed -i 's/OpenWrt/LinkSSR/g' package/base-files/files/bin/config_generate
+# Modify password
 sed -i 's/root::0:0:99999:7:::/root:\$1\$5IJBucpD\$Pq2tUZUM9KvFrW6\/zNynf\/:0:0:99999:7:::/g' package/base-files/files/etc/shadow ##密码 admin
+# Modify wifi
 sed -i 's/OpenWrt/PDCN/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
-sed -i 's/none/1234567890/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i 's/none/psk2/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i '/set wireless.default_radio\${devidx}.encryption=psk2/a \\t\t\t set wireless.default_radio\$\{devidx\}.key=1234567890' package/kernel/mac80211/files/lib/wifi/mac80211.sh
